ddd6dda451e46e9b3fe0ce5d0dc755e195c7088a
mkdtycl <143724377+mkdtycl@users.noreply.github.com>
2025-04-29 19:51:50 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -142,8 +142,7 @@ function install_command_line() {
 
   # Upgrade pip, setuptools, and wheel
   echo "Upgrading pip, setuptools, and wheel..."
-  pip install --upgrade pip setuptools
-  pip install "wheel==0.41.2"
+  pip install --upgrade pip setuptools "wheel==0.41.2" &> /dev/null
 
   # Proceed to install the cli package
   echo "Installing CRMint CLI package..."
